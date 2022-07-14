5f45f6e34971832d7a7e82a1a3356711918e3a4b
jlomako <71858760+jlomako@users.noreply.github.com>
2022-07-14 08:23:44 -0400

    Update app.R
    changed order of plots

--- app.R
+++ app.R
@@ -12,64 +12,51 @@ library(shiny)
 library(vroom)
 library(tidyverse)
 
-# use data from my pdfscraper
-data <- vroom::vroom("https://github.com/jlomako/pdfscraper/raw/main/data/daily_data.csv")
+# use data from my repository
+data <- vroom::vroom("https://github.com/jlomako/pdfscraper/raw/main/data/daily_data.csv", show_col_types = FALSE)
 
-# get hospital names
+# get values
 hospitals <- names(data[2:22])
-# get max value (for plot)
 max_value <- max(data[,2:22], na.rm=T)
 max_date <- max(data$Date)
+# get hospital name with todays max occupancy
+selected_hospital <- names(which.max(data[length(data$Date),2:21]))
+if (is.na(selected_hospital)) { selected_hospital <- "Total Montréal" } 
 
-# start shiny app:
+
 ui <- bootstrapPage(
   
   # uses bootstrap 5
   theme = bslib::bs_theme(version = 5, bootswatch = "spacelab"),
-  # tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "https://bootswatch.com/5/spacelab/bootstrap.min.css")),
-  div(class="container-fluid shadow-none p-4 mb-5 bg-light",
+  div(class="container-fluid py-4",
+      
+      # plot today
+      div(class="row",
+          div(class="col-12", 
+              h2("Occupancy rates in Montreal emergency rooms"),
+              plotOutput("plot_today"))
+      ),
+      
+      # select hospital to show occupancy rate over time
       div(class="row pt-3",
           div(class="col-12", 
-              h2("Occupancy rates in Montreal emergency rooms", class="pb-3"),
               selectInput(inputId = "hospital", 
-                          label = "Select a hospital", 
+                          label = "Select hospital to show occupancy rate over time*", 
                           choices = hospitals,
-                          selected = "Total Montréal",
+                          selected = selected_hospital,
                           width = "100%")
           )
       ),
       div(class="row",
           div(class="col-12", plotOutput("plot"))
       ),
-      # plot today
-      div(class="row",
-          div(class="col-12 pt-3", 
-              h2("Today's occupancy"),
-              plotOutput("plot_today"))
-      ),
   )
 )
 
-# back end
-server <- function(input, output, session) {
 
-  # plot select hospital output  
-  selected <- reactive(data %>% select(Date, occupancy = input$hospital))
-  output$plot <- renderPlot({
-    selected() %>%
-      ggplot(aes(Date, occupancy, fill = occupancy)) +
-      geom_col(position = "identity", size = 0.5, show.legend = F, na.rm = T)  +
-      scale_x_date(date_labels = "%b %d", date_breaks = "1 week", minor_breaks = "1 day") +
-      scale_y_continuous(limits = c(0,max_value), labels = scales::percent_format(scale = 1)) +
-      theme_minimal() +
-      labs(y = "Occupancy rate\n", x = NULL) +
-      theme(panel.grid.major.y = element_line()) + # horizontal lines only
-      scale_fill_gradient2(low = "light green", high = "red", mid = "yellow", midpoint = 80) +
-      geom_hline(yintercept = 100, col = "red")
-  }, res = 96)
+server <- function(input, output, session) {
   
-  
-  # plot output_today
+  # output_today
   output$plot_today <- renderPlot({
     data %>%
       mutate(Date = as.character(Date)) %>%
@@ -80,16 +67,33 @@ server <- function(input, output, session) {
       ggplot(aes(x = reorder(name, value), y = value, fill = value)) +
       geom_col(position = "identity", size = 0.5, show.legend = F) +
       geom_text(aes(label = paste0(value,"%")), hjust = 1, colour = "white", size = 3) +
-      scale_y_continuous(limits = c(0,max_value), labels = scales::percent_format(scale = 1)) +
+      # scale_y_continuous(limits = c(0,max_value), labels = scales::percent_format(scale = 1)) +
       coord_flip() +
       scale_fill_gradient2(low = "light green", high = "red", mid = "yellow", midpoint = 80) + 
       theme_minimal() +
-      labs(x = NULL, y = NULL, caption = paste("\nlast update: ", max_date)) +
-      theme(panel.grid.minor = element_blank())
+      labs(x = NULL, y = NULL, caption = paste("last update: ", max_date)) +
+      theme(panel.grid = element_blank(), axis.ticks.x = element_blank(), axis.text.x = element_blank())
   }, res = 96)
   
+
+  # select hospital output  
+  selected <- reactive(data %>% select(Date, occupancy = input$hospital))
+  output$plot <- renderPlot({
+    selected() %>%
+      ggplot(aes(Date, occupancy, fill = occupancy)) +
+      geom_col(position = "identity", size = 0.5, show.legend = F, na.rm = T)  +
+      scale_x_date(date_labels = "%b %d", date_breaks = "1 week", minor_breaks = "1 day") +
+      scale_y_continuous(limits = c(0,max_value), labels = scales::percent_format(scale = 1)) +
+      theme_minimal() +
+      labs(y = "Occupancy rate\n", x = NULL, caption = "*occupancy rate at 1 p.m.") +
+      theme(panel.grid.major.y = element_line()) + # horizontal lines only
+      scale_fill_gradient2(low = "light green", high = "red", mid = "yellow", midpoint = 80) +
+      geom_hline(yintercept = 100, col = "red")
+  }, res = 96)
   
-  
+  # to do: output table 
+
 }
 
 shinyApp(ui, server)
+
