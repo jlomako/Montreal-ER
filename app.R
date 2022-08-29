21962cb3f8deefaf71e230f397024edf0c570969
jlomako <71858760+jlomako@users.noreply.github.com>
2022-08-29 13:00:46 -0400

    Update app.R
    new look!

--- app.R
+++ app.R
@@ -9,11 +9,16 @@
 # install.packages("shiny")
 
 library(shiny)
-library(tidyverse)
-library(vroom)
+library(dplyr)
+library(ggplot2)
+library(stringr)
 
 # get data from repository
 data <- vroom::vroom("https://github.com/jlomako/hospital-occupancy-tracker/raw/main/data/hospitals.csv", show_col_types = FALSE)
+
+# get last 90 days
+data <- data %>% filter(Date >= (Sys.Date()-90))
+
 max_date <- max(data$Date)
 max_value <- max(data[,2:23], na.rm=T)
 names(data)[names(data) == "Total"] <- "Total Montréal"
@@ -53,46 +58,57 @@ ui <- bootstrapPage(
   
   # uses bootstrap 5
   theme = bslib::bs_theme(version = 5, bootswatch = "spacelab"),
-  div(class="container px-3",
-      
-      # plot today
-      div(class="row",
-          div(class="col-lg-10", 
-              h2("Occupancy rates in Montreal emergency rooms"),
-              plotOutput("plot_today"))
+  
+  tags$head(HTML("<title>Occupancy rates in Montréal emergency rooms</title>")),
+  
+  #  div(class="container-md",
+  
+  h1("Occupancy rates in Montréal emergency rooms", class="text-center pt-3"),
+  
+  # plot today
+  div(class="row pt-5",
+      div(class="col-lg-12",
+          div(class="card",
+              div(class="card-header bg-primary", h5("Current Occupancy Rate in Montréal ERs", class="card-title")),
+              div(class="card-body",
+                  # h5("Current Occupancy Rate in Montréal ERs", class="card-title"),
+                  plotOutput("plot_today")
+              ),
+          ),    
       ),
-      
-      # occupancy rates over time
-      div(class="row",
-          div(class="col-lg-10", 
-              h2("Occupancy rates over time"),
-              h2("Select a hospital to show occupancy rate over the last few weeks*", class="small"))
+  ),
+  
+  # occupancy rates over time
+  div(class="row pt-5",
+      div(class="col-lg-12",
+          div(class="card",
+              div(class="card-header bg-primary", h5("ER Occupancy Rates over the last 90 days", class="card-title")),
+              div(class="card-body",
+                  # h5("ER Occupancy Rates over the last 90 days", class="card-title"),
+                  
+                  div(selectInput(inputId = "hospital", 
+                                  label = "Select a hospital", #NULL 
+                                  choices = hospitals,
+                                  width = "100%")
+                  ),
+                  
+                  plotOutput("plot")
+                  
+              ),
+          ),
       ),
-      
-      # select hospital to show occupancy rate over time
-      div(class="row pt-3",
-          div(class="col-lg-10", 
-              selectInput(inputId = "hospital", 
-                          label = NULL, 
-                          choices = hospitals,
-                          # selected = selected_hospital,
-                          width = "100%")
-          )
-      ),
-      div(class="row",
-          div(class="col-lg-10", plotOutput("plot"))
-      ),
-      
-      # source & disclaimer
-      div(class="row",
-          div(class="col-lg-10 pt-6",
-              h1(" ", class = "pt-6"),
-              h6("Source: Ministère de la Santé et des Services sociaux du Québec", class="small text-center")),
-              h6("© Copyright 2022, jlomako", class="small text-center"),
-          h1(" ", class = "pb-6"),
-      ),
-      
-  )
+  ),
+  
+  # source & disclaimer
+  div(class="row",
+      div(class="col-lg-12 pt-6",
+          h1(" ", class = "pt-6"),
+          h6("Source: Ministère de la Santé et des Services sociaux du Québec", class="small text-center")),
+      h6("© Copyright 2022, jlomako", class="small text-center"),
+      h1(" ", class = "pb-6"),
+  ),
+  
+  #  ),
 )
 
 
@@ -104,10 +120,10 @@ server <- function(input, output, session) {
       filter(hospital_name != "Total Montréal") %>%
       mutate(occupancy_rate = ifelse(is.na(occupancy_rate), -0.01, occupancy_rate)) %>%
       ggplot(aes(x = reorder(hospital_name, occupancy_rate), y = occupancy_rate, fill = occupancy_rate)) +
-      geom_col(position = "identity", size = 0.5, show.legend = F) +
+      geom_col(position = "identity", size = 3, show.legend = F) +
       geom_text(aes(label = if_else(occupancy_rate < 0, "data not available", NULL)), colour = "grey", size = 3, hjust = "inward", na.rm=T) +
-      geom_text(aes(label = if_else(occupancy_rate >= 0 & occupancy_rate <= 69, paste0(occupancy_rate,"%"), NULL)), colour = "grey", size = 3, hjust = -0.2, na.rm=T) +
-      geom_text(aes(label = if_else(occupancy_rate > 69, paste0(occupancy_rate,"%"), NULL)), colour = "white", size = 3, hjust = 1, na.rm=T) +
+      geom_text(aes(label = if_else(occupancy_rate >= 0 & occupancy_rate <= 79, paste0(occupancy_rate,"%"), NULL)), colour = "darkgrey", size = 3, hjust = -0.1, position = position_stack(vjust = 0), na.rm=T) +
+      geom_text(aes(label = if_else(occupancy_rate > 79, paste0(occupancy_rate,"%"), NULL)), colour = "white", size = 3, hjust = -0.1, position = position_stack(vjust = 0), na.rm=T) +
       coord_flip() +
       scale_fill_gradient2(low = "green", high = "red", mid = "#ffff66", midpoint = 60) + 
       theme_minimal() +
@@ -116,16 +132,17 @@ server <- function(input, output, session) {
   }, res = 96, height = "auto")
   
   
-  # select hospital output  
+  # select hospital output
+  # get last 90 days
   selected <- reactive(data %>% select(Date, occupancy = input$hospital))
   output$plot <- renderPlot({
     selected() %>%
       ggplot(aes(Date, occupancy, fill = occupancy)) +
       geom_line(size = 0.5, show.legend = F, na.rm = T) +
-      scale_x_date(date_labels = "%b %d", date_breaks = "1 week", minor_breaks = "1 day") +
+      scale_x_date(date_labels = "%a, %b %d", date_breaks = "1 week", minor_breaks = "1 day") +
       scale_y_continuous(limits = c(0,max_value), labels = scales::percent_format(scale = 1)) +
       theme_minimal() +
-      labs(y = "Occupancy rate*\n", x = NULL, caption = "*occupancy rates at 12 a.m. every day") +
+      labs(y = "Occupancy rate*\n", x = NULL, caption = "\n*occupancy rates at 12 a.m. every day") +
       geom_hline(yintercept = 100, col = "red") +
       theme(axis.text.x = element_text(angle = 90, hjust = 1))
   }, res = 96)
